b1a4add8347f623ca653656026c707066fa883ba
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2020-04-13 21:37:45 +0100

    add video record

--- app/src/main/java/com/pt/fccn/educast/MainActivity.kt
+++ app/src/main/java/com/pt/fccn/educast/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.pt.fccn.educast
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_VIDEO_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<ImageButton>(R.id.video_icon)
+        button.setOnClickListener {
+            Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+                takeVideoIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_VIDEO_CAPTURE && intent!!.data != null){
+            Toast.makeText(this, "Vídeo criado com sucesso", Toast.LENGTH_LONG).show()
+        }
     }
 }
